4f31adf7ed9833f4ef06cc5317a535a142baa377
mitalrs <mitalsapkale1999@gmail.com>
2023-06-16 19:10:06 +0530

    endpoint run perfectness

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-// const routes = require("./routes/authRoutes.js");
-const connect = require("./databse/mongodb.js");
+const routes = require("./routes/indexRoutes.js");
+const connect = require("./databse/mongoDb.js");
 const app = express();
 
 app.use(express.json());
@@ -11,9 +11,9 @@ app.use(express.static("public"))
 
 const  PORT = process.env.PORT || 1333;
 
-// app.use("/api/v1", routes)
+app.use("/api/v1", routes)
 connect();
 
 app.listen(PORT, () => {
   console.log('runnig', PORT)
-})
+});
